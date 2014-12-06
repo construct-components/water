5749760aa4dc64aff5078d679df1a3cb5c604f50
Makis Tracend <makis.tracend@gmail.com>
2014-12-05 23:39:29 -0800

    init view class

--- src/main.js
+++ src/main.js
@@ -1,7 +1,7 @@
 (function() {
 	// Creates an object based in the HTML Element prototype
 	var el = Object.create(HTMLElement.prototype);
-	var $3d = false;
+	var view, $3d = false;
 
 	// Fires when an instance of the element is created
 	el.createdCallback = function() {};
@@ -9,12 +9,12 @@
 	// Fires when an instance was inserted into the document
 	el.attachedCallback = function() {
 
+		view = new APP.Elements.Water();
 		// get 3d reference
-		$3d = $(this).closest("construct");
+		//$3d = $(this).closest(".construct").three();
 		// create plane
-		//$3d.
 		// add water shader
-console.log( $3d );
+
 	};
 
 	// Fires when an instance was removed from the document
